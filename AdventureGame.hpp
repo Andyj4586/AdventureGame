f33dffe582916835b14249e64383aed44fd5bc9d
Andrew Young <andrew.young778@gmail.com>
2023-08-04 13:12:22 -0500

    consolidated files

--- AdventureGame.hpp
+++ AdventureGame.hpp
@@ -4,3 +4,13 @@ std::string choice1;
 std::string choice2;
 std::string choice3;
 
+bool keep_playing = true;
+void start_game();
+std::string step_1();
+std::string step_2G();
+std::string step_2P();
+void step_3G();
+void step_3P();
+bool play_again();
+std::string ans1;
+std::string ans2;
